b92e4a318c9366503b562b2ffd4b1a131d8c3cf9
nuclearthinking <onifent@gmail.com>
2020-08-14 02:08:01 +0300

    added an ability to shooting, but dont work properly

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -11,7 +11,7 @@ public class HeroController : MonoBehaviour
     public float movementSpeed;
 
     private Animator animator;
-    private Rigidbody2D _rb;
+    public Rigidbody2D rb;
 
     private bool _playerMoving;
     public Vector2 lastMove;
@@ -22,7 +22,6 @@ public class HeroController : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        _rb = GetComponent<Rigidbody2D>();
         if (!_playerExists)
         {
             _playerExists = true;
@@ -37,7 +36,7 @@ public class HeroController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_rb.velocity.magnitude > 0)
+        if (rb.velocity.magnitude > 0)
         {
             _playerMoving = true;
         }
@@ -51,26 +50,14 @@ public class HeroController : MonoBehaviour
 
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime, 0f, 0f));
-            // _rb.velocity =
-                // new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, _rb.velocity.y);
             _playerMoving = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime, 0f));
-            // _rb.velocity =
-                // new Vector2(_rb.velocity.x, Input.GetAxisRaw("Vertical") * movementSpeed);
             _playerMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
-        }
-
-
-        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
-        {
-            // _rb.velocity = new Vector2(_rb.velocity.x, 0f);
         }
 
         animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
@@ -82,6 +69,6 @@ public class HeroController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rb.MovePosition(_rb.position + movement * (movementSpeed * Time.fixedDeltaTime));
+        rb.MovePosition(rb.position + movement * (movementSpeed * Time.fixedDeltaTime));
     }
 }
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform firePoint;
+    public GameObject arrowPrefab;
+    public float arrowForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Shoot();    
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * arrowForce, ForceMode2D.Impulse);
+    }
+}
