58e62ea6d9dcfc91850d2be232ec658507d92148
nuclearthinking <onifent@gmail.com>
2020-08-19 22:08:56 +0300

    pixel perfect camera

---
+++ Assets/Scripts/Player/PlayerModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModel
+{
+    public int currentHealth;
+    public int maximumHealth;
+    public float movementSpeed;
+    public Vector2 lastMove;
+}
