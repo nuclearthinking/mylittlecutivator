ff77f182cd768853ed0fc8c7403e142a3b13f04f
nuclearthinking <onifent@gmail.com>
2020-08-22 19:49:58 +0300

    скриптики

---
+++ Assets/Scripts/Gameplay/PlayerShooting.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class PlayerShooting : Simulation.Event<PlayerShooting>
+    {
+
+        public PlayerView playerView;
+        public override void Execute()
+        {
+            
+            Debug.Log("Player SHOOTING");
+        }
+    }
+}
---
+++ Assets/Scripts/Mechanics/GameController.cs
@@ -0,0 +1,28 @@
+﻿using Core;
+using Model;
+using UnityEngine;
+
+namespace Mechanics
+{
+    public class GameController : MonoBehaviour
+    {
+        public static GameController Instance { get; private set; }
+
+        public GameModel model = Simulation.GetModel<GameModel>();
+
+        private void OnEnable()
+        {
+            Instance = this;
+        }
+
+        private void OnDisable()
+        {
+            if (Instance == this) Instance = null;
+        }
+
+        private void Update()
+        {
+            if (Instance == this) Simulation.Tick();
+        }
+    }
+}
