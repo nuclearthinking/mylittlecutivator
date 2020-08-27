b8ef237c17f9e02f52ca5deb9de4f492919693c6
nuclearthinking <onifent@gmail.com>
2020-08-27 23:09:02 +0300

    главное меню

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void LoadMainScene()
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
